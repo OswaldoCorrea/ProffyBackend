34571abfae8b5e2354fdafc13fe987fcbea9856a
Oswaldo Correa Neto <oswaldo.neto@shift.com.br>
2020-08-07 08:53:37 -0300

    Implementação de listar o total de conexões e pacote cors

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ const connectionsController = new ConnectionsController();
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
-//routes.post('/connections', connectionsController.index);
+routes.get('/connections', connectionsController.index);
 routes.post('/connections', connectionsController.create);
 
 export default routes;
